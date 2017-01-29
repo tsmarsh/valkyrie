87b2872262bca5e3b3582fa13b5b0c0e78e8fd9d
tmarsh <ts.marsh@gmail.com>
2017-01-29 09:36:18 -0500

    Better tests for core

--- core/src/test/java/com/tailoredshapes/valkyrie/core/StreamableResponseBodyTest.java
+++ core/src/test/java/com/tailoredshapes/valkyrie/core/StreamableResponseBodyTest.java
@@ -3,16 +3,32 @@ package com.tailoredshapes.valkyrie.core;
 import com.tailoredshapes.stash.Stash;
 import org.junit.Test;
 
+import javax.annotation.Resource;
+import java.io.BufferedWriter;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Collection;
 
 import static com.tailoredshapes.stash.Stash.stash;
+import static com.tailoredshapes.underbar.Die.die;
+import static com.tailoredshapes.underbar.Die.rethrow;
+import static com.tailoredshapes.underbar.IO.file;
+import static com.tailoredshapes.underbar.IO.resource;
+import static com.tailoredshapes.underbar.IO.responseWriter;
+import static com.tailoredshapes.underbar.UnderBar.list;
+import static com.tailoredshapes.valkyrie.core.StreamableResponseBody.extend;
 import static com.tailoredshapes.valkyrie.core.StreamableResponseBody.writeBodyToStream;
+import static com.tailoredshapes.valkyrie.util.Response.getCharset;
 import static org.junit.Assert.*;
 
 /**
  * Created by tmarsh on 11/4/16.
  */
 public class StreamableResponseBodyTest {
+
+
     @Test
     public void canWriteStrings() throws Exception {
         ByteArrayOutputStream output = new ByteArrayOutputStream();
@@ -29,5 +45,46 @@ public class StreamableResponseBodyTest {
         writeBodyToStream(response.get("body"), response, output);
 
         assertEquals("Hello, World!", output.toString("UTF-16"));
+    }
+
+    @Test
+    public void canWriteOutputStreams() throws Exception {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        Stash response = stash("body", new ByteArrayInputStream("Hello, World!".getBytes("UTF-8")), "headers", stash());
+        writeBodyToStream(response.get("body"), response, output);
+
+        assertEquals("Hello, World!", output.toString("UTF-8"));
+    }
+
+    @Test
+    public void canWriteCollections() throws Exception {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        Stash response = stash("body", list("Hello, ", "World", '!'), "headers", stash());
+        writeBodyToStream(response.get("body"), response, output);
+
+        assertEquals("Hello, World!", output.toString("UTF-8"));
+    }
+
+    @Test
+    public void canWriteFiles() throws Exception {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        Stash response = stash("body", file(resource("/test.txt")), "headers", stash());
+        writeBodyToStream(response.get("body"), response, output);
+
+        assertEquals("Hello, World!", output.toString("UTF-8"));
+    }
+
+    @Test
+    public void canBeExtendedArbitrarily() throws Exception {
+        extend(URL.class, (body, response, outputStream) -> {
+            writeBodyToStream(file(body), response, outputStream);
+        });
+
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+        Stash response = stash("body", resource("/test.txt"), "headers", stash());
+        writeBodyToStream(response.get("body"), response, output);
+
+        assertEquals("Hello, World!", output.toString("UTF-8"));
     }
 }
