9c981fe98c89008d9e12a6784a3f4a76a17f38b1
tmarsh <ts.marsh@gmail.com>
2017-01-31 08:50:09 -0500

    Programatic KeyStore test

--- jetty-adapter/src/test/java/com/tailoredshapes/valkyrie/adapters/JettyTest.java
+++ jetty-adapter/src/test/java/com/tailoredshapes/valkyrie/adapters/JettyTest.java
@@ -13,6 +13,7 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
+import java.security.KeyStore;
 import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
 
@@ -62,6 +63,56 @@ public class JettyTest {
                         "ssl?", true,
                         "ssl-port", 5533,
                         "keystore", file(resource("/keystore")).getAbsolutePath(),
+                        "key-password", "p@ssW0rd!",
+                        "host", "localhost"));
+
+        server.start();
+
+        TrustManager[] trustAllCerts = new TrustManager[] {
+                new X509TrustManager() {
+                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                        return null;
+                    }
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {  }
+
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {  }
+                }
+        };
+
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(null, trustAllCerts, new SecureRandom());
+
+        CloseableHttpClient client = HttpClients.custom()
+                .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
+                .setSSLContext(sc)
+                .build();
+
+        HttpGet get = new HttpGet("https://localhost:5533");
+        CloseableHttpResponse response = client.execute(get);
+        assertEquals(200, response.getStatusLine().getStatusCode());
+        assertEquals("text/plain", response.getFirstHeader("Content-Type").getValue());
+        String body = EntityUtils.toString(response.getEntity());
+        assertEquals("Hello, World", body);
+
+        server.stop();
+    }
+
+    @Test
+    public void shouldStartASSLJettyServerWithAKeyStore() throws Exception {
+        KeyStore ks = KeyStore.getInstance("JCEKS");
+        ks.load(this.getClass().getResourceAsStream("/keystore"), "p@ssW0rd!".toCharArray());
+
+        Server server = runJetty((req) ->
+                        stash(
+                                "status", 200,
+                                "headers", stash("Content-Type", "text/plain"),
+                                "body", "Hello, World"),
+                stash(
+                        "join?", false,
+                        "port", 6699,
+                        "ssl?", true,
+                        "ssl-port", 5533,
+                        "keystore", ks,
                         "key-password", "p@ssW0rd!",
                         "host", "localhost"));
 
