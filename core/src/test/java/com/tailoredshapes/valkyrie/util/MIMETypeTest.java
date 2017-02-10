5fdaff5478f879d9937c8115b036bc615be87ce1
tmarsh <ts.marsh@gmail.com>
2017-02-09 21:03:13 -0500

    MIMEType utility

---
+++ core/src/test/java/com/tailoredshapes/valkyrie/util/MIMETypeTest.java
@@ -0,0 +1,26 @@
+package com.tailoredshapes.valkyrie.util;
+
+import org.junit.Test;
+
+import static com.tailoredshapes.underbar.UnderBar.hash;
+import static com.tailoredshapes.valkyrie.util.MIMEType.extMimeType;
+import static org.junit.Assert.*;
+
+/**
+ * Created by tmarsh on 2/9/17.
+ */
+public class MIMETypeTest {
+
+    @Test
+    public void shouldReturnAMimeTypeForAKnownType() throws Exception {
+        assertEquals("application/json", extMimeType("foo.json"));
+        assertEquals("text/plain", extMimeType("json"));
+        assertEquals("text/plain", extMimeType("foo.undefined"));
+    }
+
+    @Test
+    public void shouldReturnAMimeTypeForAnAdditionalType() throws Exception {
+        assertEquals("application/json", extMimeType("foo.json", hash("undefined", "foo/bar")));
+        assertEquals("foo/bar", extMimeType("foo.undefined", hash("undefined", "foo/bar")));
+    }
+}
