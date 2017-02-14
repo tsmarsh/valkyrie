88d54cfef619e8b06da4d30e830977e7b264bd6a
tmarsh <ts.marsh@gmail.com>
2017-02-13 20:46:00 -0500

    wip

--- core/src/main/java/com/tailoredshapes/valkyrie/middleware/Cookies.java
+++ core/src/main/java/com/tailoredshapes/valkyrie/middleware/Cookies.java
@@ -4,18 +4,26 @@ import com.tailoredshapes.stash.Stash;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.BooleanSupplier;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static com.tailoredshapes.stash.Stash.stash;
+import static com.tailoredshapes.underbar.Die.dieIf;
 import static com.tailoredshapes.underbar.UnderBar.*;
 import static com.tailoredshapes.underbar.UnderReg.groups;
 import static com.tailoredshapes.underbar.UnderReg.pattern;
+import static com.tailoredshapes.underbar.UnderString.join;
 import static com.tailoredshapes.valkyrie.util.Parsing.reToken;
+import static com.tailoredshapes.valkyrie.util.Parsing.value;
 import static com.tailoredshapes.valkyrie.util.Request.getHeader;
 
 public interface Cookies {
@@ -30,7 +38,7 @@ public interface Cookies {
     Pattern cookie = pattern("\\s*(" + reToken + ")=(" + cookieValue + ")\\s*[;,]?");
 
 
-    Stash setCookieAttributes = stash(
+    Map<String, String> setCookieAttributes = hash(
             "domain", "Domain",
             "max-age", "Max-Age",
             "path", "Path",
@@ -77,5 +85,76 @@ public interface Cookies {
         }
 
         return stash();
+    }
+
+    static String writeValue(String key, Object value, Function<Stash, String> encoder){
+        return encoder.apply(stash(key, value));
+    }
+
+
+    static boolean isValidAttribute(String key, Object value){
+        Predicate<String> checkValuesTypes = s -> {
+            switch (key) {
+                case "max-age":
+                    return value instanceof Instant || value instanceof Long;
+                case "expires":
+                    return value instanceof Date || value instanceof String;
+                default:
+                    return true;
+            }
+        };
+
+        return setCookieAttributes.containsKey(key) &&
+                !value.toString().contains(";") &&
+                checkValuesTypes.test(key);
+
+    }
+
+    static String writeAttrMap(Stash attributes){
+        for(String key : attributes.keys()) {
+            dieIf(!isValidAttribute(key, attributes.get(key)),
+                    () -> "cookie attributes are not valid: " + key + "" + attributes.get(key));
+        }
+
+        List<String> attrStrings = attributes.map((k, v) -> {
+            String attr = setCookieAttributes.get(k);
+            if(v instanceof Instant){
+                return ";" + attr + "=" + ((Instant) v).getEpochSecond();
+            }else if (v instanceof Date) {
+                return ";" + attr + "=" + rfc822Formatter.format((Date)v);
+            }else if (v instanceof Boolean){
+                return (Boolean)v ? ";" + attr : "";
+            } else {
+                return ";" + attr + "=" + value;
+            }
+        });
+
+        return join(attrStrings);
+    }
+
+    static List<String> writeCookies(Stash cookies, Function<Stash, String> encoder){
+        return cookies.map((k, v) -> {
+            if(v instanceof Stash){
+                return writeValue(k, v, encoder) + writeAttrMap((Stash)v);
+            } else {
+                return writeValue(k, v, encoder);
+            }
+        });
+    }
+
+    static Stash setCookies(Stash response, Function<Stash, String> encoder){
+        if(response.contains("cookies")){
+            Stash headers = response.get("headers");
+            headers.update("Set-Cookie", join(writeCookies(response.get("cookies"), encoder)));
+        }
+        return response;
+    }
+
+    static Stash cookiesRequest(Stash request){
+        return cookiesRequest(request, stash());
+    }
+
+    static Stash cookiesRequest(Stash request, Stash options){
+        options.get("decoder", )
     }
 }
