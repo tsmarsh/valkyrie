25164aaa8e8a86fccd8b7189580dd7688ecb24d5
tmarsh <ts.marsh@gmail.com>
2017-02-15 20:34:27 -0500

    Added a form encoded

---
+++ core/src/main/java/com/tailoredshapes/valkyrie/util/FormEncodeable.java
@@ -0,0 +1,87 @@
+package com.tailoredshapes.valkyrie.util;
+
+import com.tailoredshapes.stash.Stash;
+
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import static com.tailoredshapes.underbar.Die.dieIfEmpty;
+import static com.tailoredshapes.underbar.Die.rethrow;
+import static com.tailoredshapes.underbar.UnderBar.*;
+import static com.tailoredshapes.underbar.UnderString.join;
+
+public abstract class FormEncodeable {
+
+    private static final Map<Class, BiFunction<?, String, String>> impls = new HashMap<>();
+
+    static {
+        extend(String.class, (unencoded, encoding) -> rethrow(() -> URLEncoder.encode(unencoded, encoding)));
+        extend(Map.class, (params, encoding) -> {
+            Function<Object, String> encode = (x) -> formEncode(x, encoding);
+            BiFunction<Object, Object, String> encodeParam = (k, v) -> encode.apply(k) + "=" + encode.apply(v);
+            List<String> encodedParams = new ArrayList<>();
+            for (Object k : params.keySet()) {
+                Object v = params.get(k);
+
+                if (v instanceof Iterable) {
+                    Iterable mapV = (Iterable) v;
+                    encodedParams.addAll(map(mapV, u -> encodeParam.apply(k, u)));
+                } else {
+                    encodedParams.add(encodeParam.apply(k, v));
+                }
+            }
+            return join("&", encodedParams);
+        });
+
+        extend(Stash.class, (params, encoding) -> {
+            Function<Object, String> encode = (x) -> formEncode(x, encoding);
+            BiFunction<Object, Object, String> encodeParam = (k, v) -> encode.apply(k) + "=" + encode.apply(v);
+            List<String> encodedParams = new ArrayList<>();
+            params.map((k,v) -> {
+                Object val = params.get(k);
+
+                if (val instanceof Iterable) {
+                    Iterable mapV = (Iterable) val;
+                    encodedParams.addAll(map(mapV, u -> encodeParam.apply(k, u)));
+                } else {
+                    encodedParams.add(encodeParam.apply(k, val));
+                }
+
+                return v;
+            });
+
+            return join("&", encodedParams);
+        });
+    }
+
+
+    public static <T> void extend(Class<T> type, BiFunction<T, String, String> impl) {
+        synchronized (impls) {
+            impls.put(type, impl);
+        }
+    }
+
+    public static <T> String formEncode(T t, String encoding) {
+        BiFunction<T, String, String> formEncoder;
+
+        if (impls.containsKey(t.getClass())) {
+            synchronized (impls) {
+                formEncoder = (BiFunction<T, String, String>) impls.get(t.getClass());
+            }
+        } else {
+            List<Class> handlers = filter(impls.keySet(), (Class k) -> k.isAssignableFrom(t.getClass()));
+            if (isEmpty(handlers)) {
+                return formEncode(t.toString(), encoding);
+            }
+            Class targetClass = first(dieIfEmpty(handlers, () -> "No implementation for " + t.getClass().getName()));
+            formEncoder = (BiFunction<T, String, String>) impls.get(targetClass);
+        }
+
+        return formEncoder.apply(t, encoding);
+    }
+}
---
+++ core/src/test/java/com/tailoredshapes/valkyrie/util/FormEncodeableTest.java
@@ -0,0 +1,50 @@
+package com.tailoredshapes.valkyrie.util;
+
+import com.tailoredshapes.stash.Stash;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static com.tailoredshapes.stash.Stash.stash;
+import static com.tailoredshapes.underbar.UnderBar.hash;
+import static com.tailoredshapes.underbar.UnderBar.list;
+import static com.tailoredshapes.valkyrie.util.FormEncodeable.formEncode;
+import static org.junit.Assert.*;
+
+/**
+ * Created by tmarsh on 2/15/17.
+ */
+public class FormEncodeableTest {
+    @Test
+    public void shouldEncodeAString() throws Exception {
+        String test = "Приве́т नमस्ते שָׁלוֹם";
+        assertEquals("%D0%9F%D1%80%D0%B8%D0%B2%D0%B5%CC%81%D1%82+%E0%A4%A8%E0%A4%AE%E0%A4%B8%E0%A5%8D%E0%A4%A4%E0%A5%87+%D7%A9%D6%B8%D7%81%D7%9C%D7%95%D6%B9%D7%9D",
+                formEncode(test, "UTF-8"));
+    }
+
+    @Test
+    public void shouldEncodeAnObject(){
+        Object test = new Object(){
+            @Override
+            public String toString() {
+                return "Hello, World";
+            }
+        };
+
+        assertEquals("Hello%2C+World", formEncode(test, "UTF-8"));
+    }
+
+    @Test
+    public void shouldEncodeAMap() throws Exception {
+        Map<String, Object> foo = hash("foo", "bar", "eggs", list("a", "b", "c"));
+
+        assertEquals("eggs=a&eggs=b&eggs=c&foo=bar", formEncode(foo, "UTF-16"));
+    }
+
+    @Test
+    public void shouldEncodeAStash() throws Exception {
+        Stash foo = stash("foo", "bar", "eggs", list("a", "b", "c"));
+
+        assertEquals("eggs=a&eggs=b&eggs=c&foo=bar", formEncode(foo, "UTF-16"));
+    }
+}
